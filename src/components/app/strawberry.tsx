73998c62c96bbb9a0dcfdd0e40350b118738cf26
Muhammad Zourdy <69587933+zourdyzou@users.noreply.github.com>
2022-07-07 09:59:09 +0200

    Update strawberry.tsx

--- src/components/app/strawberry.tsx
+++ src/components/app/strawberry.tsx
@@ -1,7 +1,5 @@
 import StrawberryIcon from "@images/strawberry.component.svg";
-/**
- * Created by: Andrey Polyakov (andrey@polyakov.im)
- */
+
 import React, { CSSProperties } from "react";
 
 export default ({ style, className }: { style?: CSSProperties; className?: string }): React.ReactElement => (
