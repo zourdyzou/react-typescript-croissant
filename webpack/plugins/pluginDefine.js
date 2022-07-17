6cf92952880bbc89710efc6e5bba28d97e56038e
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-07-17 12:13:19 +0200

    Add dotenv configuration to repo

--- webpack/plugins/pluginDefine.js
+++ webpack/plugins/pluginDefine.js
@@ -1,4 +1,4 @@
-import {DefinePlugin} from 'webpack';
+import { DefinePlugin } from "webpack";
 
 /**
  * Created by: Andrey Polyakov (andrey@polyakov.im)
@@ -7,15 +7,12 @@ import {DefinePlugin} from 'webpack';
  *     isProd: true
  * }
  */
-import {isDev, isDevServer, isProd, mode} from '../utils/env';
+import { isDev, isDevServer, isProd } from "../utils/env";
 
 const config = {
-    'process.env': {
-        NODE_ENV: JSON.stringify(mode),
-    },
-    IS_PROD: isProd,
-    IS_DEV: isDev,
-    IS_DEV_SERVER: isDevServer,
+  IS_PROD: isProd,
+  IS_DEV: isDev,
+  IS_DEV_SERVER: isDevServer,
 };
 
 export const definePlugin = new DefinePlugin(config);
